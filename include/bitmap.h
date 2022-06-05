6b49c081fd8ed9cb9e340a77cfbf6c921763274e
Sean Phillips <seanphillips141@outlook.com>
2022-06-05 13:16:45 -0400

    Moved implementation from bitmap header to seperate c source file.

--- include/bitmap.h
+++ include/bitmap.h
@@ -1,17 +1,7 @@
 #ifndef BITMAP_H
 #define BITMAP_H
 
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <byteswap.h>
-
 #include "matrix.h"
-
-#define BMP_FILE_HEADER_SIZE (uint8_t)(14) // 14 bytes long
-#define BMP_INFO_HEADER_SIZE (uint8_t)(40) // 40 bytes long
-#define BMP_COLR_HEADER_SIZE (uint8_t)(84) // 84 bytes long
 
 struct BMPFileHeader
 {
@@ -22,59 +12,11 @@ struct BMPFileHeader
     uint8_t offset_data[4];
 };
 
-struct BMPFileHeader *allocate_bmpfileheader()
-{
-    struct BMPFileHeader *ptr = (struct BMPFileHeader *)malloc(sizeof(struct BMPFileHeader));
+struct BMPFileHeader *allocate_bmpfileheader();
 
-    if (ptr == NULL)
-    {
-        printf("allocate_bmpfileheader: Failed to allocate BMPFileHeader.\n");
-        return NULL;
-    }
+void deallocate_bmpfileheader(struct BMPFileHeader *bmpFileHeaderPtr);
 
-    uint16_t *file_type = (uint16_t *)ptr->file_type;
-    *file_type = 0x4D42;
-
-    uint32_t *file_size = (uint32_t *)ptr->file_size;
-    // *file_size = 0x0000008E;
-    *file_size = 0x00000000;
-
-    uint16_t *reserved_1 = (uint16_t *)ptr->reserved_1;
-    *reserved_1 = 0x0000;
-
-    uint16_t *reserved_2 = (uint16_t *)ptr->reserved_2;
-    *reserved_2 = 0x0000;
-
-    uint32_t *offset_data = (uint32_t *)ptr->offset_data;
-    *offset_data = 0x0000008A;
-
-    return ptr;
-}
-
-void deallocate_bmpfileheader(struct BMPFileHeader *bmpFileHeaderPtr)
-{
-    if (bmpFileHeaderPtr == NULL)
-        return;
-    free(bmpFileHeaderPtr);
-}
-
-void set_bmpfileheader_filesize(const struct matrix *mat, struct BMPFileHeader *bmpFileHeaderPtr)
-{
-    if (mat == NULL)
-    {
-        printf("set_bmpfilesize: mat passed in is NULL.\n");
-        return;
-    }
-
-    if (bmpFileHeaderPtr == NULL)
-    {
-        printf("set_bmpfilesize: bmpFileHeader passed in is NULL.\n");
-        return;
-    }
-
-    uint32_t *file_size = (uint32_t *)bmpFileHeaderPtr->file_size;
-    *file_size = BMP_FILE_HEADER_SIZE + BMP_INFO_HEADER_SIZE + BMP_COLR_HEADER_SIZE + (mat->horizontal * mat->vertical * 4);
-}
+void set_bmpfileheader_filesize(const struct matrix *mat, struct BMPFileHeader *bmpFileHeaderPtr);
 
 struct BMPInfoHeader
 {
@@ -96,81 +38,11 @@ struct BMPInfoHeader
     uint8_t colors_important[4];
 };
 
-struct BMPInfoHeader *allocate_bmpinfoheader()
-{
-    struct BMPInfoHeader *ptr = (struct BMPInfoHeader *)malloc(sizeof(struct BMPInfoHeader));
+struct BMPInfoHeader *allocate_bmpinfoheader();
 
-    if (ptr == NULL)
-    {
-        printf("allocate_bmpinfoheader: Failed to allocate BMPInfoHeader.\n");
-        return NULL;
-    }
+void deallocate_bmpinfoheader(struct BMPInfoHeader *bmpInfoHeaderPtr);
 
-    uint32_t *header_size = (uint32_t *)ptr->header_size;
-    *header_size = 0x0000007C;
-
-    int32_t *width = (int32_t *)ptr->width;
-    // *width = 0x00000001;
-    *width = 0x00000000;
-
-    int32_t *height = (int32_t *)ptr->height;
-    // *height = 0x00000001;
-    *height = 0x00000000;
-
-    uint16_t *planes = (uint16_t *)ptr->planes;
-    *planes = 0x0001;
-
-    uint16_t *bit_count = (uint16_t *)ptr->bit_count;
-    *bit_count = 0x0010;
-
-    uint32_t *compression = (uint32_t *)ptr->compression;
-    *compression = 0x00000000;
-
-    uint32_t *size_image = (uint32_t *)ptr->size_image;
-    *size_image = 0x00000004;
-
-    uint32_t *x_pixels_per_meter = (uint32_t *)ptr->x_pixels_per_meter;
-    *x_pixels_per_meter = 0x00002E23;
-
-    uint32_t *y_pixels_per_meter = (uint32_t *)ptr->y_pixels_per_meter;
-    *y_pixels_per_meter = 0x00002E23;
-
-    uint32_t *color_used = (uint32_t *)ptr->colors_used;
-    *color_used = 0x00000000;
-
-    uint32_t *colors_important = (uint32_t *)ptr->colors_important;
-    *colors_important - 0x00000000;
-
-    return ptr;
-}
-
-void deallocate_bmpinfoheader(struct BMPInfoHeader *bmpInfoHeaderPtr)
-{
-    if (bmpInfoHeaderPtr == NULL)
-        return;
-    free(bmpInfoHeaderPtr);
-}
-
-void set_bmpinfoheader_dimensions(const struct matrix *mat, struct BMPInfoHeader *bmpInfoHeaderPtr)
-{
-    if (mat == NULL)
-    {
-        printf("set_bmp_width_height: mat passed in is NULL.\n");
-        return;
-    }
-
-    if (bmpInfoHeaderPtr == NULL)
-    {
-        printf("set_bmp_width_height: bmpInfoHeaderPtr passed in is NULL.\n");
-        return;
-    }
-
-    int32_t *width = (int32_t *)bmpInfoHeaderPtr->width;
-    *width = mat->horizontal;
-
-    int32_t *height = (int32_t *)bmpInfoHeaderPtr->height;
-    *height = mat->vertical;
-}
+void set_bmpinfoheader_dimensions(const struct matrix *mat, struct BMPInfoHeader *bmpInfoHeaderPtr);
 
 struct BMPColorHeader
 {
@@ -183,140 +55,10 @@ struct BMPColorHeader
     uint8_t unused[64];
 };
 
-struct BMPColorHeader *allocate_bmpcolorheader()
-{
-    struct BMPColorHeader *ptr = (struct BMPColorHeader *)malloc(sizeof(struct BMPColorHeader));
+struct BMPColorHeader *allocate_bmpcolorheader();
 
-    uint32_t *red_mask = (uint32_t *)ptr->red_mask;
-    *red_mask = 0x0000F800;
+void deallocate_bmpcolorheader(struct BMPColorHeader *bmpColorHeaderPtr);
 
-    uint32_t *green_mask = (uint32_t *)ptr->green_mask;
-    *green_mask = 0x000007E0;
-
-    uint32_t *blue_mask = (uint32_t *)ptr->blue_mask;
-    *blue_mask = 0x0000001F;
-
-    uint32_t *alpha_mask = (uint32_t *)ptr->alpha_mask;
-    *alpha_mask = 0x00000000;
-
-    uint32_t *color_space_type = (uint32_t *)ptr->color_space_type;
-    *color_space_type = 0x73524742;
-
-    for (uint8_t index = 0; index < 64; index++)
-    {
-        ptr->unused[index] = 0x00;
-    }
-
-    return ptr;
-}
-
-void deallocate_bmpcolorheader(struct BMPColorHeader *bmpColorHeaderPtr)
-{
-    if (bmpColorHeaderPtr == NULL)
-        return;
-    free(bmpColorHeaderPtr);
-}
-
-void write_rgb565_bmpfile(const char *filepath, struct matrix *mat)
-{
-    struct BMPFileHeader *header_ptr = allocate_bmpfileheader();
-    struct BMPInfoHeader *info_ptr = allocate_bmpinfoheader();
-    struct BMPColorHeader *color_ptr = allocate_bmpcolorheader();
-
-    // TODO: This causes memory leaks
-    if (header_ptr == NULL)
-    {
-        printf("Unable to allocate BMPFileHeader.\n");
-        goto cleanup_file_header;
-    }
-
-    if (info_ptr == NULL)
-    {
-        printf("Unable to allocate BMPInfoHeader.\n");
-        goto cleanup_info_header;
-    }
-
-    if (color_ptr == NULL)
-    {
-        printf("Unable to allocate BMPColorHeader.\n");
-        goto cleanup_everything;
-    }
-
-    set_bmpfileheader_filesize(mat, header_ptr);
-    set_bmpinfoheader_dimensions(mat, info_ptr);
-
-    FILE *fileptr = fopen(filepath, "wb");
-    if (fileptr == NULL)
-    {
-        printf("Unable to open %s for binary writing.\n", filepath);
-        goto cleanup_everything;
-    }
-
-    fwrite(header_ptr, sizeof(struct BMPFileHeader), 1, fileptr);
-    fwrite(info_ptr, sizeof(struct BMPInfoHeader), 1, fileptr);
-    fwrite(color_ptr, sizeof(struct BMPColorHeader), 1, fileptr);
-
-    uint16_t working_data = 0x0000;
-    uint16_t green_data = 0x0000;
-    uint16_t red_data = 0x0000;
-    uint16_t blue_data = 0x0000;
-    uint16_t red_color = 0x0000;
-    uint16_t green_color = 0x0000;
-    uint16_t blue_color = 0x0000;
-    uint16_t padding = 0x0000;
-
-    uint32_t index = 0;
-    uint32_t row_raw_length = 0;
-    uint32_t row_length = mat->horizontal * mat->channel;
-    // int toggle_padding = (ceil((16.0 * mat->horizontal) / 32.0) * 4) - (mat->horizontal * 2);
-    int toggle_padding = ((mat->horizontal % 2) == 0) ? 0 : 1;
-    for (int32_t row = (mat->vertical - 1); row >= 0; row--)
-    {
-        index = row * row_length;
-        row_raw_length = (row+1) * row_length;
-        while (index < row_raw_length)
-        {
-            working_data = 0x0000;
-
-            red_color = mat->mem[index];
-            red_color = bswap_16(red_color);
-            red_data = 0x0000;
-            red_data |= ((red_color << 3) & 0xF800);
-
-            // Likely a better way to write this
-            green_color = mat->mem[index + 1] & 0x003F;
-            green_color = bswap_16(green_color);
-            green_data = 0x0000;
-            green_data |= ((green_color << 2) | (green_color >> 8)) & 0xE007;
-            green_data = bswap_16(green_data);
-
-            blue_color = mat->mem[index + 2];
-            blue_color = bswap_16(blue_color);
-            blue_data = 0x0000;
-            blue_data |= ((blue_color >> 6) & 0x00F8);
-
-            working_data = (green_data | red_data | blue_data);
-            working_data &= 0xFFFF;
-
-            fwrite(&working_data, sizeof(uint16_t), 1, fileptr);
-
-            index += mat->channel;
-        }
-
-        if (toggle_padding)
-            fwrite(&padding, sizeof(uint16_t), 1, fileptr);
-
-        index = 0;
-    }
-
-    fclose(fileptr);
-
-cleanup_everything:
-    deallocate_bmpcolorheader(color_ptr);
-cleanup_info_header:
-    deallocate_bmpinfoheader(info_ptr);
-cleanup_file_header:
-    deallocate_bmpfileheader(header_ptr);
-}
+void write_rgb565_bmpfile(const char *filepath, struct matrix *mat);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,24 +4,39 @@
 
 int main()
 {
-    printf("Hello world!\n");
-
-    struct matrix *mat = allocate_matrix(JPG_FORMAT, 2, 3);
+    struct matrix *mat = allocate_matrix(JPG_FORMAT, 24, 24);
     if (mat == NULL)
     {
         printf("Unable to allocate matrix.\n");
         return 0;
     }
-    // write_rgb565_pixel_code(0x07ff, mat, 0, 0);
-    // write_rgb565_pixel_rgb(0xFF, 0x00, 0x00, mat, 0, 0);
+
     write_rgb565_pixel_rgb(0xFF, 0x00, 0x00, mat, 0, 0);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 1, 1);
+    write_rgb565_pixel_rgb(0x00, 0x00, 0xFF, mat, 2, 2);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 3, 3);
+    write_rgb565_pixel_rgb(0xFF, 0x00, 0x00, mat, 4, 4);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 5, 5);
+    write_rgb565_pixel_rgb(0x00, 0x00, 0xFF, mat, 6, 6);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 7, 7);
+    write_rgb565_pixel_rgb(0xFF, 0x00, 0x00, mat, 8, 8);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 9, 9);
+    write_rgb565_pixel_rgb(0x00, 0x00, 0xFF, mat, 10, 10);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 11, 11);
+    write_rgb565_pixel_rgb(0xFF, 0x00, 0x00, mat, 12, 12);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 13, 13);
+    write_rgb565_pixel_rgb(0x00, 0x00, 0xFF, mat, 14, 14);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 15, 15);
+    write_rgb565_pixel_rgb(0xFF, 0x00, 0x00, mat, 16, 16);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 17, 17);
+    write_rgb565_pixel_rgb(0x00, 0x00, 0xFF, mat, 18, 16);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 19, 15);
+    write_rgb565_pixel_rgb(0xFF, 0x00, 0x00, mat, 20, 14);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 21, 13);
+    write_rgb565_pixel_rgb(0x00, 0x00, 0xFF, mat, 22, 12);
+    write_rgb565_pixel_rgb(0x00, 0xFF, 0x00, mat, 23, 11);
 
-    // write_rgb565_pixel_rgb(0xFF, 0xFF, 0xFF, mat, 1, 1);
-    // write_rgb565_pixel_rgb(0xFF, 0xFF, 0xFF, mat, 1, 0);
-
-    // print_matrix(mat);
-
-    const char* test_path = "application.bmp";
+    const char* test_path = "application_2.bmp";
 
     write_rgb565_bmpfile(test_path, mat);
 
