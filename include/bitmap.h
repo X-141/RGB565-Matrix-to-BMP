5e6f626ce6d88386c525c91ce5981e65e3c1ab0b
Sean Phillips <seanphillips141@outlook.com>
2022-06-21 22:12:19 -0400

    Added typedefs for bitmap structs.

--- include/bitmap.h
+++ include/bitmap.h
@@ -7,13 +7,13 @@
  * @brief General information for the image processor to help it understand how to 
  * begin reading our BMP file.
  */
-struct BMPFileHeader {
+typedef struct BMPFileHeader {
   uint8_t file_type[2];
   uint8_t file_size[4];
   uint8_t reserved_1[2];
   uint8_t reserved_2[2];
   uint8_t offset_data[4];
-};
+} BMPFileHeader;
 
 /**
  * @brief Allocate a new BMP file header for BMP file generation. If 
@@ -21,14 +21,14 @@ struct BMPFileHeader {
  * 
  * @return struct BMPFileHeader* 
  */
-struct BMPFileHeader *allocate_bmpfileheader();
+BMPFileHeader* allocate_bmpfileheader();
 
 /**
  * @brief Deallocate existing BMP file header structure.
  * 
  * @param bmpFileHeaderPtr Pointer to an existing Bitmap File Header.
  */
-void deallocate_bmpfileheader(struct BMPFileHeader *bmpFileHeaderPtr);
+void deallocate_bmpfileheader(BMPFileHeader *bmpFileHeaderPtr);
 
 /**
  * @brief Sets the filesize field of the Bitmap Filesize header.
@@ -37,13 +37,13 @@ void deallocate_bmpfileheader(struct BMPFileHeader *bmpFileHeaderPtr);
  * @param bmpFileHeaderPtr Pointer to existing File Header
  */
 void set_bmpfileheader_filesize(const struct matrix *mat,
-                                struct BMPFileHeader *bmpFileHeaderPtr);
+                                BMPFileHeader *bmpFileHeaderPtr);
 
 /**
  * @brief Structure to represent the context of the image (what exactly are the
  * dimensions, size, how many bits per pixel, etc).
  */
-struct BMPInfoHeader {
+typedef struct BMPInfoHeader {
   uint8_t header_size[4];
 
   int8_t width[4];
@@ -60,7 +60,7 @@ struct BMPInfoHeader {
 
   uint8_t colors_used[4];
   uint8_t colors_important[4];
-};
+} BMPInfoHeader;
 
 /**
  * @brief Allocate a new BMP info header for file generation.
@@ -69,14 +69,14 @@ struct BMPInfoHeader {
  * 
  * @return struct BMPInfoHeader* 
  */
-struct BMPInfoHeader *allocate_bmpinfoheader();
+BMPInfoHeader *allocate_bmpinfoheader();
 
 /**
  * @brief Deallocate existing info header stucture.
  * 
  * @param bmpInfoHeaderPtr Pointer to existing BMP info header.
  */
-void deallocate_bmpinfoheader(struct BMPInfoHeader *bmpInfoHeaderPtr);
+void deallocate_bmpinfoheader(BMPInfoHeader *bmpInfoHeaderPtr);
 
 /**
  * @brief Set the fields in the info header to specify the dimensions of the matrix.
@@ -85,19 +85,19 @@ void deallocate_bmpinfoheader(struct BMPInfoHeader *bmpInfoHeaderPtr);
  * @param bmpInfoHeaderPtr Pointer to existing info header
  */
 void set_bmpinfoheader_dimensions(const struct matrix *mat,
-                                  struct BMPInfoHeader *bmpInfoHeaderPtr);
+                                  BMPInfoHeader *bmpInfoHeaderPtr);
 
 /**
  * @brief Header stucture that informs the image processor how to read in the pixel data.
  */
-struct BMPColorHeader {
+typedef struct BMPColorHeader {
   uint8_t red_mask[4];
   uint8_t green_mask[4];
   uint8_t blue_mask[4];
   uint8_t alpha_mask[4];
 
   uint8_t unused[68];
-};
+} BMPColorHeader;
 
 /**
  * @brief Allocate a new BMP color header for file generation.
@@ -106,14 +106,14 @@ struct BMPColorHeader {
  * 
  * @return struct BMPColorHeader* 
  */
-struct BMPColorHeader *allocate_bmpcolorheader();
+BMPColorHeader *allocate_bmpcolorheader();
 
 /**
  * @brief Deallocate bmp color header.
  * 
  * @param bmpColorHeaderPtr Pointer to an existing BMP color header
  */
-void deallocate_bmpcolorheader(struct BMPColorHeader *bmpColorHeaderPtr);
+void deallocate_bmpcolorheader(BMPColorHeader *bmpColorHeaderPtr);
 
 /**
  * @brief Given a matrix containing arbitrary dimensions and data, write the data out 
